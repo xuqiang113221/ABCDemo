707c59993af0145e63c94dec10de8c97c758a582
xuqiang <495219869@qq.com>
2014-08-04 10:39:54 +0800

    my firstVersion

---
+++ ABCDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ABCDemo
+//
+//  Created by xuqiang on 14-8-4.
+//  Copyright (c) 2014年 xuqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ABCDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ABCDemo
+//
+//  Created by xuqiang on 14-8-4.
+//  Copyright (c) 2014年 xuqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
